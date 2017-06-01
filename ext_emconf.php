4656a26b4aa2aeaf87523f586dfb089be1f66a0d
Tim Nhan Ta <TimTa@Tims-MacBook-Pro.local>
2017-06-01 12:07:38 +0200

    Fix spaces for composer.json and ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,28 +1,28 @@
 <?php
 $EM_CONF[$_EXTKEY] = [
-    'title' => 'Importify',
-    'description' => 'A generic importer for TYPO3',
-    'category' => 'be',
-    'author' => 'Tim Ta',
-    'author_email' => 'tim.ta@pixelant.se',
-    'state' => 'alpha',
-    'internal' => '',
-    'uploadfolder' => '0',
-    'createDirs' => '',
-    'clearCacheOnLoad' => 1,
-    'version' => '0.0.0',
-    'constraints' => [
-        'depends' => [
-            'typo3' => '8.7.0-8.9.99',
-        ],
-        'conflicts' => [
-        ],
-        'suggest' => [
-        ],
+  'title' => 'Importify',
+  'description' => 'A generic importer for TYPO3',
+  'category' => 'be',
+  'author' => 'Tim Ta',
+  'author_email' => 'tim.ta@pixelant.se',
+  'state' => 'alpha',
+  'internal' => '',
+  'uploadfolder' => '0',
+  'createDirs' => '',
+  'clearCacheOnLoad' => 1,
+  'version' => '0.0.0',
+  'constraints' => [
+    'depends' => [
+      'typo3' => '8.7.0-8.9.99',
     ],
-    'autoload' => [
-        'classmap' => [
-            'Classes'
-        ]
+    'conflicts' => [
+    ],
+    'suggest' => [
+    ],
+  ],
+  'autoload' => [
+    'classmap' => [
+      'Classes'
     ]
+  ]
 ];
